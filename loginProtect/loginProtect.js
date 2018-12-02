8976dba34a6af5fd88ef17aa7002c8b72ad92a9a
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2018-12-02 14:19:28 -0800

    In this file, we had to use closures to perform the function if ip function matches the username and password and then return the result of the fucntion.

--- loginProtect/loginProtect.js
+++ loginProtect/loginProtect.js
@@ -10,8 +10,27 @@ with the 3rd and 4th argument that are passed to your closure
 Use the tests to guide your solution
 */
 
-const loginProtect = function () {
+/*
+Write a function called loginProtect
+This function will take 3 arguments
+1. A username
+2. A password
+3. A function that will take two arguments
+Your function should return a closure that when invoked 
+with the correct username and password will invoke your third argument
+with the 3rd and 4th argument that are passed to your closure 
+Use the tests to guide your solution
+*/
 
-}
-
-module.exports = loginProtect
+var loginProtect = function (username , password, func) {
+  return function(usname , ipPass){
+  if(password === ipPass && username === usname) {
+    return func(arguments[2], arguments[3])
+  }
+  };
+};
+var myFunc = function(a, b) {
+  console.log('my func ran');
+  return a + b;
+};
+var loginProtection = loginProtect( 'mehul','p@ssw0rd',myFunc);
